229ccbc277e778edd767d1ac35ea6ae9e5928d00
Muja Mo Ismail <muja@foss.land>
2022-01-30 18:42:26 +0100

    added date and time

--- config.def.h
+++ config.def.h
@@ -65,5 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+        { separator,        "|",             NULL },
+        { datetime,         "  %s",       "%b %d (%a) %T " },
 };
